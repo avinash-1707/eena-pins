4830c58a82db39f8390fa0b42d4b3792311850a4
Avinash <avinashnarwariya5@gmail.com>
2026-02-11 17:15:26 +0530

    fixed create product schema

--- src/schema/createProductSchema.ts
+++ src/schema/createProductSchema.ts
@@ -1,22 +1,22 @@
 import z from "zod";
 
 export const createProductSchema = z.object({
-    name: z.string().min(1),
-    price: z.number().int().nonnegative(),
-    imageUrl: z.string(),
-    category: z.string().min(1),
-    description: z.string().min(1),
-    warrantyMonths: z.number().int().positive().optional(),
-    freeShippingOn: z.boolean().optional(),
-    returnAvailable: z.boolean().optional(),
+  name: z.string().min(1),
+  price: z.number().int().nonnegative(),
+  imageUrl: z.string(),
+  category: z.string().min(1),
+  description: z.string().min(1),
+  warrantyMonths: z.number().int().positive().optional(),
+  freeShippingOn: z.boolean().optional(),
+  returnAvailable: z.boolean().optional(),
 
-    details: z
-        .object({
-            brand: z.string().optional(),
-            material: z.string().optional(),
-            keyFeatures: z.array(z.string()),
-            moreOptions: z.record(z.string(), z.string()).optional(),
-            rating: z.number().min(0).max(5).optional(),
-        })
-        .optional(),
+  details: z
+    .object({
+      brand: z.string().optional(),
+      material: z.string().optional(),
+      keyFeatures: z.array(z.string()).optional(),
+      moreOptions: z.record(z.string(), z.string()).optional(),
+      rating: z.number().min(0).max(5).optional(),
+    })
+    .optional(),
 });
